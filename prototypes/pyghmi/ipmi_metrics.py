b3107cf9aefcd9e0e5ed9f788f77f9df24b3a876
Daniel Brewer <dcbrewer@ncsu.edu>
2017-10-22 18:37:05 +0000

    update

--- prototypes/pyghmi/ipmi_metrics.py
+++ prototypes/pyghmi/ipmi_metrics.py
@@ -1,6 +1,6 @@
 from pyghmi.ipmi import command
 
-ipmi = command.Command(bmc='eb2tvx02drac.csc.ncsu.edu', userid='admin', password='/*hoRV7or2C')
-sensors = ipmi.get_sensor_descriptions()
+ipmi = command.Command(bmc='eb2-2214-sd01ipmi.csc.ncsu.edu', userid='admin', password='sdteam18')
+sensors = ipmi.get_sensor_data()
 for s in sensors:
 	print s
