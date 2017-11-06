92073428110d12efbe07aafa4b47ddc822dc698b
root <root@sd-vm12.csc.ncsu.edu>
2017-11-06 16:29:46 +0000

    got script for running standalone PollerManager working

--- main/model/Poller.py
+++ main/model/Poller.py
@@ -21,6 +21,7 @@ class Poller:
             t_start = time.time()
             for server_connection in self.server_connection_list:
                 server_connection.poll_connection_and_save_usage_report()
+                print str(t_start) + ": scan @ " + server_connection.usage_report_factory.unique_id + " (" + server_connection.usage_report_factory.ipmi.bmc + ")"
                 if self.stopped:
                     break
             t_end = time.time()
--- main/run_standalone_pollermanager.py
+++ main/run_standalone_pollermanager.py
@@ -1,17 +1,17 @@
-from PollerManager import PollerManager
+from model.PollerManager import PollerManager
 from model.JobManager import JobManager
 from model.Job import Host
 from model.Job import Job
 import sys
 
 job_id = 1
-interval = sys.argv[1]
+interval = float(sys.argv[1])
 host_list = []
-#host_list.append(Host("eb2tvx02drac.csc.ncsu.edu", "admin", "/*hoRV7or2C", "dell"))
+host_list.append(Host("eb2tvx02drac.csc.ncsu.edu", "admin", "/*hoRV7or2C", "dell"))
 host_list.append(Host("eb2-2214-sd01ipmi.csc.ncsu.edu", "admin", "sdteam18", "cisco"))
 job = Job(job_id, interval, host_list)
 
 
 poller_manager = PollerManager()
-print "PollerManager created - " + str(poller_manager) + "/n"
+print "PollerManager created - " + str(poller_manager)
 poller_manager.start_job(job)
